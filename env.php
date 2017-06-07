39c49a8969d9963bc1182b9c7e4b33c9ec7c872b
Bart Kooijman <kooijmanbj@gmail.com>
2017-06-08 01:37:50 +0200

    Removed defined check

--- env.php
+++ env.php
@@ -7,5 +7,5 @@ $dotenv->required(['DB_DSN', 'DB_USERNAME', 'DB_PASSWORD']);
 $dotenv->required('YII_DEBUG')->allowedValues([true, false]);
 $dotenv->required('YII_ENV')->allowedValues(['dev', 'prod', 'test']);
 
-defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
+define('YII_DEBUG', getenv('YII_DEBUG'));
+define('YII_ENV', getenv('YII_ENV'));
