8c205bc2e689e72b2108e5c13647f8d8813329eb
Bart Kooijman <kooijmanbj@gmail.com>
2017-06-08 01:36:33 +0200

    Define in env.php

--- env.php
+++ env.php
@@ -6,3 +6,6 @@ $dotenv->load();
 $dotenv->required(['DB_DSN', 'DB_USERNAME', 'DB_PASSWORD']);
 $dotenv->required('YII_DEBUG')->allowedValues([true, false]);
 $dotenv->required('YII_ENV')->allowedValues(['dev', 'prod', 'test']);
+
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
--- web/index.php
+++ web/index.php
@@ -1,11 +1,7 @@
 <?php
 
 require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__.'/../env.php');
-
-defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
-
+require(__DIR__ . '/../env.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
