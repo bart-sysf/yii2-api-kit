a1b887809b176e23f80e220dddbd8c8c0e213cf3
Bart Kooijman <kooijmanbj@gmail.com>
2017-06-09 12:58:47 +0200

    AutoController with gii-models and apidoc actions.

---
+++ commands/AutoController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace app\commands;
+
+use Yii;
+use yii\console\Controller;
+use yii\console\Exception;
+use yii\helpers\Console;
+
+class AutoController extends Controller
+{
+    const YES_VALUES = ['y', 'Y', 'yes', 'Yes'];
+    const ACTIONS = ['gii-models', 'apidoc'];
+
+    private $continue = false;
+    private $npmBin = '';
+
+    public function init()
+    {
+        parent::init();
+
+        exec('npm bin', $this->npmBin);
+        if (!$this->npmBin)
+            throw new Exception('Please check if Node and it\'s packages are installed correctly.');
+    }
+
+    public function beforeAction($action)
+    {
+        if (YII_ENV_PROD && !$this->continue) {
+            $continue = Console::prompt('This action should not be run in production. Continue? (y/n)', ['default' => 'n']);
+            if (!in_array($continue, $this::YES_VALUES))
+                return false;
+
+            $this->continue = true;
+        }
+
+        return parent::beforeAction($action);
+    }
+
+    public function actionAll()
+    {
+        foreach($this::ACTIONS as $action) {
+            $this->runAction($action);
+        }
+    }
+
+    public function actionGiiModels()
+    {
+        $tableNames = Yii::$app->db->schema->tableNames;
+
+        foreach ($tableNames as $tableName) {
+            $mainClass = str_replace(' ', '', ucwords(str_replace('_', ' ', $tableName)));
+            $modelClass = 'Base' . $mainClass;
+            $this->run('gii/model', ['baseClass' => 'BaseActiveRecord', 'modelClass' => $modelClass, 'tableName' => $tableName, 'overwrite' => true, 'interactive' => false]);
+
+            $mainFile = '@app/models/' . $mainClass;
+            if (!file_exists($mainFile)) {
+                $body = <<<PHP
+                    <?php
+
+                    namespace app/models;
+
+                    class {$mainClass} extends {$modelClass} {
+
+                    }
+
+PHP;
+                $handle = fopen($mainFile, 'w');
+                fwrite($handle, $body);
+                fclose($handle);
+            }
+        }
+    }
+
+    public function actionApidoc()
+    {
+        exec('"' . $this->npmBin[0] . '\apidoc" -i .\controllers -o .\web\apidoc');
+    }
+}
