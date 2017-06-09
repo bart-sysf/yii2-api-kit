65631d168bec74dd4fbbc438a293161f621eb629
Bart Kooijman <kooijmanbj@gmail.com>
2017-06-09 14:06:18 +0200

    AccountController prep

---
+++ controllers/AccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use yii\rest\Controller;
+
+class AccountController extends Controller
+{
+    public function actionRegister()
+    {
+
+    }
+
+    public function actionLogin()
+    {
+
+    }
+}
