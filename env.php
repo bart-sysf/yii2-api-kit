583c4ff4946e5e18ab8bccbed90154ac4a8ac4c1
Bart Kooijman <kooijmanbj@gmail.com>
2017-06-09 13:09:07 +0200

    ENTRY_URL env var

--- env.php
+++ env.php
@@ -3,7 +3,7 @@
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-$dotenv->required(['DB_DSN', 'DB_USERNAME', 'DB_PASSWORD']);
+$dotenv->required(['DB_DSN', 'DB_USERNAME', 'DB_PASSWORD', 'ENTRY_URL']);
 $dotenv->required('YII_DEBUG')->allowedValues([true, false]);
 $dotenv->required('YII_ENV')->allowedValues(['dev', 'prod', 'test']);
 
@@ -12,3 +12,4 @@ define('YII_ENV', getenv('YII_ENV'));
 define('DB_DSN', getenv('DB_DSN'));
 define('DB_USERNAME', getenv('DB_USERNAME'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('ENTRY_URL', getenv('ENTRY_URL'));
